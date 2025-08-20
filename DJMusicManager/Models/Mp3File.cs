654567ec5c9caa12639994bdf8c0ca09d61f1a15
boombuster2000 <chanuka.williams@outlook.com>
2025-08-20 15:45:58 +0100

    Create model for MP3 file which can load metadata when needed to improve performance.

---
+++ DJMusicManager/Models/Mp3File.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DJMusicManager.Models;
+
+public class Mp3File(string filePath)
+{
+    private string FilePath { get; } = filePath ?? throw new ArgumentNullException(nameof(filePath));
+    public string? Title { get; set; }
+    public string[]? Artists { get; set; }
+    public TimeSpan Duration { get; private set; }
+    public string[]? Genres { get; set; }
+    public double? Bpm { get; set; }
+
+    public void LoadMetaData()
+    {
+        var file = TagLib.File.Create(FilePath);
+        Title = file.Tag.Title;
+        
+        // If performers exist, split names into separate elements
+        // By default it does not split artists names into individual elements.
+        Artists = file.Tag.Performers is { Length: > 0 } ? file.Tag.Performers[0].Split(", ") : [];
+
+        Duration = file.Properties.Duration;
+        Genres = file.Tag.Genres;
+        Bpm = file.Tag.BeatsPerMinute;
+
+    }
+}
