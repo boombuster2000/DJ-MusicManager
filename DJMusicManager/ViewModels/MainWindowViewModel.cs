a2c2fb5277fdbcf7cd59ff0b326c92ed9ea37880
boombuster2000 <chanuka.williams@outlook.com>
2025-08-25 20:53:15 +0100

    Fixed bug where old loaded folders are still viewable.

--- DJMusicManager/ViewModels/MainWindowViewModel.cs
+++ DJMusicManager/ViewModels/MainWindowViewModel.cs
@@ -11,16 +11,16 @@ namespace DJMusicManager.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    [ObservableProperty] public partial Folder SelectedFolder { get; private set; } =  new Folder();
+    [ObservableProperty] public partial Folder? SelectedFolder { get; private set; } =  null;
 
 
     [RelayCommand]
-    public Task LoadSelectedFolderCommand(IReadOnlyList<IStorageFolder> folders)
+    public Task LoadSelectedFolderCommand(IReadOnlyList<IStorageFolder> selectedFolders)
     {
-        if (folders.Count <= 0) return Task.CompletedTask;
-        
-        Debug.Print("Loading folders");
-        SelectedFolder = new Folder(folders[0].TryGetLocalPath() ?? string.Empty);
+        if (selectedFolders.Count <= 0) return Task.CompletedTask;
+
+        SelectedFolder = null;
+        SelectedFolder = new Folder(selectedFolders[0].TryGetLocalPath() ?? string.Empty);
         
         return Task.CompletedTask;
     }
