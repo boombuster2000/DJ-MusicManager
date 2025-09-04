1ff482e3d8de2d7cab37fe27cd4e72a4600c2802
boombuster2000 <chanuka.williams@outlook.com>
2025-09-04 22:00:23 +0100

    Properties of Mp3File is now observerable property so UI can update when its loaded.

--- DJMusicManager/Models/Mp3File.cs
+++ DJMusicManager/Models/Mp3File.cs
@@ -1,38 +1,45 @@
 ﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace DJMusicManager.Models;
 
-public class Mp3File(string filePath)
+public partial class Mp3File(string filePath) : ObservableObject
 {
     /// <summary>
     /// This is the file-path of the mp3 file.
     /// </summary>
     private string FilePath { get; } = filePath;
-    
+
     /// <summary>
     /// This is the title of the mp3 file.
     /// </summary>
-    public string? Title { get; set; }
+    [ObservableProperty]
+    public partial string? Title { get; set; }
     
     /// <summary>
     /// The artists that are part of the song.
     /// </summary>
-    public string[]? Artists { get; set; }
+    ///
+    [ObservableProperty]
+    public partial string[]? Artists { get; set; }
     
     /// <summary>
     /// How long the song is.
     /// </summary>
-    public TimeSpan Duration { get; private set; }
+    [ObservableProperty]
+    public partial TimeSpan Duration { get; private set; }
     
     /// <summary>
     /// The genres of the song would fit into.
     /// </summary>
-    public string[]? Genres { get; set; }
+    [ObservableProperty]
+    public partial string[]? Genres { get; set; }
     
     /// <summary>
     /// The bpm of the song.
     /// </summary>
-    public double? Bpm { get; set; }
+    [ObservableProperty]
+    public partial double? Bpm { get; set; }
 
     public void LoadMetaData()
     {
