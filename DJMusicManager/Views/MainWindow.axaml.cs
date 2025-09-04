451ddebd8d16a7643daa384bd28eaeabb4d6c9c7
boombuster2000 <chanuka.williams@outlook.com>
2025-09-04 21:57:26 +0100

    Triggers mp3 meta data to load of selected folder.

--- DJMusicManager/Views/MainWindow.axaml.cs
+++ DJMusicManager/Views/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
+using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
+using DJMusicManager.Models;
 using DJMusicManager.ViewModels;
 
 namespace DJMusicManager.Views;
@@ -27,5 +29,19 @@ public partial class MainWindow : Window
             });
         
         _ = (DataContext as MainWindowViewModel)?.LoadSelectedFolderCommand(selectedFolders);
+    }
+
+    private void TreeView_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (e.AddedItems.Count <= 0) return;
+        // Get the first newly selected item
+        var selected = e.AddedItems[0];
+
+        // If your items are of type Folder
+        if (selected is Folder folder)
+        {
+            (DataContext as MainWindowViewModel)?.LoadMp3MetaDataCommand();
+        }
+
     }
 }
