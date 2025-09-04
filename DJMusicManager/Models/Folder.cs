62f04334b11874712bfce61e9ba87ae23e2b7aa1
boombuster2000 <chanuka.williams@outlook.com>
2025-09-04 15:51:39 +0100

    Made Folder not nullable.

--- DJMusicManager/Models/Folder.cs
+++ DJMusicManager/Models/Folder.cs
@@ -16,6 +16,12 @@ public class Folder
     public ObservableCollection<Mp3File> Mp3Files { get; } = [];
 
     private bool _isLoaded;
+
+    public Folder()
+    {
+        Name = "";
+        FullPath = "";
+    }
     
     public Folder(string rootPath)
     {
--- DJMusicManager/ViewModels/MainWindowViewModel.cs
+++ DJMusicManager/ViewModels/MainWindowViewModel.cs
@@ -11,15 +11,14 @@ namespace DJMusicManager.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    [ObservableProperty] public partial Folder? SelectedFolder { get; private set; } =  null;
+    [ObservableProperty] public partial Folder SelectedFolder { get; private set; } =  new Folder();
 
 
     [RelayCommand]
     public Task LoadSelectedFolderCommand(IReadOnlyList<IStorageFolder> selectedFolders)
     {
         if (selectedFolders.Count <= 0) return Task.CompletedTask;
-
-        SelectedFolder = null;
+        
         SelectedFolder = new Folder(selectedFolders[0].TryGetLocalPath() ?? string.Empty);
         
         return Task.CompletedTask;
