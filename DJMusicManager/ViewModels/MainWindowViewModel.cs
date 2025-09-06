e9f6d9ca7a8413ea550fe39085ea1cefc0a24dbd
boombuster2000 <chanuka.williams@outlook.com>
2025-09-06 14:12:56 +0100

    Loading metadata automatically when SelectedFolder changes with binding from treeview.

--- DJMusicManager/ViewModels/MainWindowViewModel.cs
+++ DJMusicManager/ViewModels/MainWindowViewModel.cs
@@ -16,27 +16,24 @@ public partial class MainWindowViewModel : ViewModelBase
 
 
     [ObservableProperty] public partial Folder SelectedFolder { get; set; } = new Folder();
+    partial void OnSelectedFolderChanged(Folder value)
+    {
+        _ = SelectedFolder.LoadMp3sParallelAsync();
+    }
     
     /// <summary>
     /// Updates <see cref="LoadedFolder"/> with new selected folder which updates the UI.
     /// </summary>
-    /// <param name="selectedFolders">The selected folders from the user/pop-up window.</param>
+    /// <param name="folderToLoad">The selected folders from the user/pop-up window.</param>
     /// <returns></returns>
     [RelayCommand]
-    public Task LoadSelectedFolderCommand(IReadOnlyList<IStorageFolder> selectedFolders)
+    public Task LoadFolderCommand(IReadOnlyList<IStorageFolder> folderToLoad)
     {
-        if (selectedFolders.Count <= 0) return Task.CompletedTask;
+        if (folderToLoad.Count <= 0) return Task.CompletedTask;
         
-        LoadedFolder = new Folder(selectedFolders[0].TryGetLocalPath() ?? string.Empty);
+        LoadedFolder = new Folder(folderToLoad[0].TryGetLocalPath() ?? string.Empty);
         
         return Task.CompletedTask;
     }
-
-
-    [RelayCommand]
-    public void LoadMp3MetaDataCommand()
-    {
-        SelectedFolder.LoadMp3s();
-    }
-
+    
 }
